4f55d5f3498bba2ca9b2036456f71d0b5b2da159
Ales Ban <alesbah04@gmail.com>
2023-12-09 23:51:28 +0300

    initial var2 lab6 change project title

--- Вариант_2/Lab6/Lab6/TvShopApp/App.xaml.cs
+++ Вариант_2/Lab6/Lab6/TvShopApp/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Lab6_TvShopApp
+namespace TvShopApp
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- Вариант_2/Lab6/Lab6/TvShopApp/MainWindow.xaml.cs
+++ Вариант_2/Lab6/Lab6/TvShopApp/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Lab6_TvShopApp
+namespace TvShopApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
